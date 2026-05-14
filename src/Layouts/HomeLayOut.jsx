2b548917f7b3656db1ba7d0b52780b03834b2039
Tarek Hossen <tarekhossenpc@gmail.com>
2026-05-14 15:07:27 +0600

    sidebar updated

--- src/Layouts/HomeLayOut.jsx
+++ src/Layouts/HomeLayOut.jsx
@@ -19,11 +19,11 @@ const HomeLayOut = () => {
         </nav>
       </header>
       <main className="w-11/12 mx-auto my-2 grid grid-cols-12 gap-5">
-        <aside className="col-span-3"><LeftAside></LeftAside></aside>
+        <aside className="col-span-3 sticky top-2 h-fit"><LeftAside></LeftAside></aside>
         <section className="main col-span-6">
           <Outlet></Outlet>
         </section>
-        <aside className="col-span-3"><RightAside></RightAside></aside>
+        <aside className="col-span-3 sticky top-2 h-fit"><RightAside></RightAside></aside>
       </main>
     </div>
   );
