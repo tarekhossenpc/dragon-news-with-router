780b51bf4f121a767f627a5edc8112031725d066
Tarek Hossen <tarekhossenpc@gmail.com>
2026-05-14 15:27:30 +0600

    change

--- src/Layouts/HomeLayOut.jsx
+++ src/Layouts/HomeLayOut.jsx
@@ -19,11 +19,11 @@ const HomeLayOut = () => {
         </nav>
       </header>
       <main className="w-11/12 mx-auto my-2 grid grid-cols-12 gap-5">
-        <aside className="col-span-3 sticky top-2 h-fit"><LeftAside></LeftAside></aside>
+        <aside className="col-span-3 sticky top-0 h-fit"><LeftAside></LeftAside></aside>
         <section className="main col-span-6">
           <Outlet></Outlet>
         </section>
-        <aside className="col-span-3 sticky top-2 h-fit"><RightAside></RightAside></aside>
+        <aside className="col-span-3 sticky top-0 h-fit"><RightAside></RightAside></aside>
       </main>
     </div>
   );
